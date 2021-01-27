a2f40c8fcced09372047e43fd21ff0198e919e39
Jerome Copeland <76133780+Jerhombus@users.noreply.github.com>
2021-01-26 23:59:13 -0700

    first commit

---
+++ module.py
@@ -0,0 +1,16 @@
+def jvol():
+	jheight=int(input("Input height"))
+	jwidth=int(input("Input width"))
+	jlength=int(input("Input length"))
+	return (jheight*jwidth*jlength)
+    
+print(jvol())
+
+
+import math
+def jcir():
+	jrad=(int(input("Input Radius")))
+	
+	return (2*math.pi*jrad)
+    
+print(jcir())
